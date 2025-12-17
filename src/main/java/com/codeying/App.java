7d5687ba294c5f52b8e0ad66766726b900bfb40d
threetwoa <laiyif68@gmail.com>
2025-12-17 12:13:50 +0800

    test_complete_pull&push_isAccessible

--- src/main/java/com/codeying/App.java
+++ src/main/java/com/codeying/App.java
@@ -9,19 +9,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @MapperScan("com.codeying.mapper")
-公共 class App {
+class App {
 
     //文件上传路径
     public static String fileUploadPath = System.getProperty("user.dir") + "\\fileUpload\\";
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
-    }
-    void contextLoads(){
-        System.out.println("hello world-aaa");
-    }
-    void contextLoads_isPUll(){
-        System.out.println("hello world-bbb");
     }
     
 }
